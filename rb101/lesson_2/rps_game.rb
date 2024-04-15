d250bf3ab16fa633ee554c2788b7592577b9d324
Elchunen Freund <elchu@macbook-pro.home>
2024-04-14 22:36:16 -0400

    fixed up again

--- rb101/lesson_2/rps_game.rb
+++ rb101/lesson_2/rps_game.rb
@@ -1,50 +1,76 @@
-VALID_CHIOCES = ['rock', 'paper', 'scissors']
+VALID_CHOICES = ['rock', 'paper', 'scissors', 'spock', 'lizard']
 
 def prompt(message)
   puts "=> #{message}"
 end
 
-def win?(first, second)
-  (first == 'rock' && second == 'scissors') ||
-    (first == 'paper' && second == 'rock') ||
-    (first == 'scissors' && second == 'paper')
+MOVES = { rock: ['scissors', 'lizard'],
+          paper: ['rock', 'spock'],
+          scissors: ['paper', 'lizard'],
+          lizard: ['spock', 'paper'],
+          spock: ['scissors', 'rock'] }
+
+def win?(hash, first, second)
+  hash[first.to_sym].include?(second)
 end
 
-def display_result(player, computer)
-  if win?(player, computer)
-    prompt('Congratz man, you won!!')
-  elsif win?(computer, player)
-    prompt('You lost to a computer, dummy')
+def display_result(array, player, computer)
+  if win?(array, player, computer)
+    'player'
+  elsif win?(array, computer, player)
+    'computer'
   else
     prompt("It's a tie")
   end
 end
 
 choice = ''
-player_wins = 0
-computer_wins = 0
+player_score = 0
+computer_score = 0
 
 loop do
   loop do
-    prompt("Choose one: #{VALID_CHIOCES.join(', ')}")
-    choice = gets.chomp
+    loop do
+      prompt("Choose one: #{VALID_CHOICES.join(', ')}")
+      choice = gets.chomp
 
-    if VALID_CHIOCES.include?(choice.downcase)
+      if VALID_CHOICES.include?(choice.downcase)
+        break
+      else
+        prompt("thats not a valid choice.")
+      end
+    end
+
+    computer_choice = VALID_CHOICES.sample
+
+    prompt("You chose #{choice}, and the computer choose #{computer_choice}.")
+
+    result = display_result(MOVES, choice, computer_choice)
+
+    case result
+    when 'player'
+      player_score += 1
+    when 'computer'
+      computer_score += 1
+    end
+
+    prompt("Your score is #{player_score}; computer score is #{computer_score}")
+
+    if player_score == 3
+      prompt('you are the grand winner')
       break
-    else
-      prompt("thats not a valid choice.")
+    elsif computer_score == 3
+      prompt('you are the grand looser')
+      break
     end
   end
-
-  computer_choice = VALID_CHIOCES.sample
-
-  prompt("You chose #{choice}, and the computer choose #{computer_choice}.")
-
-  display_result(choice, computer_choice)
 
   prompt('Do you want to play again')
   answer = gets.chomp
   break unless answer.downcase.start_with?('y')
+
+  player_score = 0
+  computer_score = 0
 end
 
 prompt("Thank you for playing, Goodbye!")
