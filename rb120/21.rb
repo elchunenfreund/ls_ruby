46f0981aca556746bd15f63d67b28ce96de4edbf
Elchunen Freund <elchunenfreund@gmail.com>
2024-12-25 16:09:23 -0500

    Started working on the 21 game

---
+++ rb120/21.rb
@@ -0,0 +1,43 @@
+class Player
+  def initialize
+
+  end
+
+  def hit
+  end
+
+  def stay
+  end
+
+  def busted?
+  end
+
+  def total
+  end
+end
+
+class Participant
+
+end
+
+class Deck
+  def initialize
+  end
+
+  def deal
+  end
+end
+
+class Card
+  def initialize
+  end
+end
+
+class Game
+  def initialize
+  end
+end
+
+Game.new.start
+
+
