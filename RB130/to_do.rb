a8d9c038c2dfce56a4545e919fd4cc9a6db70a2b
Elchunen Freund <elchunenfreund@gmail.com>
2025-02-26 00:25:00 -0500

    worked on more methods

--- RB130/to_do.rb
+++ RB130/to_do.rb
@@ -37,98 +37,93 @@ class Todo
   end
 end
 
-
 # This class represents a collection of Todo objects.
 # You can perform typical collection-oriented actions
 # on a TodoList object, including iteration and selection.
 
 class TodoList
-  attr_accessor :title, :todos
+  attr_accessor :title
 
   def initialize(title)
     @title = title
     @todos = []
   end
 
-  def add(obj)
-    raise TypeError, "Can only add Todo objects" unless obj.is_a?(Todo)
-
-    todos << obj
-    todos
-  end
-
-  def <<(obj)
-    add(obj)
-  end
-
   def size
-    todos.size
+    @todos.size
   end
 
   def first
-    todos.first
+    @todos.first
   end
 
   def last
-    todos.last
-  end
-
-  def to_a
-    todos
-  end
-
-  def done?
-    todos.all? { |todo| todo.done? }
-  end
-
-  def item_at(int)
-    raise IndexError if int > (todos.size - 1)
-    todos[int]
-  end
-
-  def mark_done_at(int)
-    raise IndexError if int > (todos.size - 1)
-    todos[int].done!
-  end
-
-  def mark_undone_at(int)
-    raise IndexError if int > (todos.size - 1)
-    todos[int].undone!
-  end
-
-  def done!
-    todos.map(&:done!)
+    @todos.last
   end
 
   def shift
-    todos.shift
+    @todos.shift
   end
 
   def pop
-    todos.pop
+    @todos.pop
   end
 
-  def remove_at(int)
-    raise IndexError if int > (todos.size - 1)
-    todos.delete_at(int)
+  def done?
+    @todos.all? { |todo| todo.done? }
+  end
+
+  def <<(todo)
+    raise TypeError, 'can only add Todo objects' unless todo.instance_of? Todo
+
+    @todos << todo
+  end
+  alias_method :add, :<<
+
+  def item_at(idx)
+    @todos.fetch(idx)
+  end
+
+  def mark_done_at(idx)
+    item_at(idx).done!
+  end
+
+  def mark_undone_at(idx)
+    item_at(idx).undone!
+  end
+
+  def done!
+    @todos.each_index do |idx|
+      mark_done_at(idx)
+    end
+  end
+
+  def remove_at(idx)
+    @todos.delete(item_at(idx))
   end
 
   def to_s
-    puts " ---- #{self.title} ---- "
-    todos.each do |todo|
-      puts todo
+    text = "---- #{title} ----\n"
+    text << @todos.map(&:to_s).join("\n")
+    text
+  end
+
+  def to_a
+    @todos.clone
+  end
+
+  def each
+    @todos.each do |todo|
+      yield(todo)
     end
+    self
+  end
+
+  def select
+    new_list = TodoList.new(title) 
+    each do |todo|
+      new_list << todo if yield(todo)
+    end
+    new_list
   end
 end
-
-todo1 = Todo.new("Buy milk")
-todo2 = Todo.new("Clean room")
-todo3 = Todo.new("Go to gym")
-list = TodoList.new("Today's Todos")
-
-list.add(todo1)                 # adds todo1 to end of list, returns list
-list.add(todo2)                 # adds todo2 to end of list, returns list
-list.add(todo3)
-
-list.to_s
-
