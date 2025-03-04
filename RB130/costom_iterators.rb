558654c262ec8b686fb0e3d27d9fb69250dc4197
Elchunen Freund <elchunenfreund@gmail.com>
2025-03-04 00:59:42 -0500

    finished with costom iterators

--- RB130/costom_iterators.rb
+++ RB130/costom_iterators.rb
@@ -1,3 +1,53 @@
+each([1, 2, 3, def each(array)
+  counter = 0
+
+  while counter < array.size
+    yield(array[counter])
+    counter += 1
+  end
+
+  array
+end4, 5]) do |num|
+  puts num
+end
+
+def select(arr)
+  counter = 0
+  new_arr = []
+
+  while counter < arr.size
+    if yield(arr[counter])
+      new_arr << arr[counter]
+    end
+    counter += 1
+  end
+
+  new_arr
+end
+
+def times(number)
+  counter = 0
+  while counter < number do
+    yield(counter)
+    counter += 1
+  end
+
+  number
+end
+
+def reduce(arr, var = 0)
+  counter = 0
+  acc = var
+
+  while counter < arr.size
+
+    acc = yield(acc, arr[counter])
+    counter += 1
+  end
+
+  acc
+end
+
 def any?(arr)
   arr.each { |obj| return true if yield(obj)}
   false
@@ -61,3 +111,58 @@ def each_with_object(arr, obj)
   arr.each { |object| yield(object, obj) }
   obj
 end
+
+def max_by(array)
+  return nil if array.empty?
+
+  max_element = array.first
+  largest = yield(max_element)
+
+  array[1..-1].each do |item|
+    yielded_value = yield(item)
+    if largest < yielded_value
+      largest = yielded_value
+      max_element = item
+    end
+  end
+
+  max_element
+end
+
+def each_cons(array, n)
+  array.each_index do |index|
+    break if index + n - 1 >= array.size
+    yield(*array[index..(index + n - 1)])
+  end
+  nil
+end
+
+hash = {}
+each_cons([1, 3, 6, 10], 1) do |value|
+  hash[value] = true
+end
+p hash == { 1 => true, 3 => true, 6 => true, 10 => true }
+
+hash = {}
+each_cons([1, 3, 6, 10], 2) do |value1, value2|
+  hash[value1] = value2
+end
+p hash == { 1 => 3, 3 => 6, 6 => 10 }
+
+hash = {}
+each_cons([1, 3, 6, 10], 3) do |value1, *values|
+  hash[value1] = values
+end
+p hash == { 1 => [3, 6], 3 => [6, 10] }
+
+hash = {}
+each_cons([1, 3, 6, 10], 4) do |value1, *values|
+  hash[value1] = values
+end
+p hash == { 1 => [3, 6, 10] }
+
+hash = {}
+each_cons([1, 3, 6, 10], 5) do |value1, *values|
+  hash[value1] = values
+end
+p hash == {}
