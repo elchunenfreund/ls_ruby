82e6a575d9f8c9111a1715287f219332ad39cf47
Elchunen Freund <elchunenfreund@gmail.com>
2025-04-11 00:57:05 -0400

    added the perfect number files

--- RB130/scrabble_score.rb
+++ RB130/scrabble_score.rb
@@ -31,4 +31,38 @@ class Scrabble
   end
 end
 
+# LS Solution
+class Scrabble
+  attr_reader :word
+
+  POINTS = {
+    'AEIOULNRST'=> 1,
+    'DG' => 2,
+    'BCMP' => 3,
+    'FHVWY' => 4,
+    'K' => 5,
+    'JX' => 8,
+    'QZ' => 10
+}
+  def initialize(word)
+    @word = word ? word : ''
+  end
+
+  def score
+    letters = word.upcase.gsub(/[^A-Z]/, '').chars
+
+    total = 0
+    letters.each do |letter|
+      POINTS.each do |all_letters, point|
+        total += point if all_letters.include?(letter)
+      end
+    end
+    total
+  end
+
+  def self.score(word)
+    Scrabble.new(word).score
+  end
+end
+
 p Scrabble.new('f').score
