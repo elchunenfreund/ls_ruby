cd62fa21a763ad94eceafa7ceb1d0c688dae60e6
Elchunen Freund <elchunenfreund@gmail.com>
2025-04-23 00:31:15 -0400

    added the last two easy exersises

---
+++ RB130/series.rb
@@ -0,0 +1,20 @@
+class Series
+  def initialize(number)
+    @number = number.split('').map(&:to_i)
+  end
+
+  def slices(int)
+    result = []
+    raise ArgumentError if int > @number.size
+
+    @number.each_cons(int) { |sub| result << sub }
+    result
+  end
+
+  def self.slices
+    self.new.slices
+  end
+end
+
+series = Series.new('81228')
+
