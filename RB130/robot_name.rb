ae0daeb8f41853893d65226dcd553706806bee18
Elchunen Freund <elchunenfreund@gmail.com>
2025-04-23 17:28:51 -0400

    added the robot_name files

---
+++ RB130/robot_name.rb
@@ -0,0 +1,27 @@
+class Robot
+  @@list_of_names = []
+  def initialize
+    @name = generate_unique_name
+  end
+
+  def generate_unique_name
+    loop do
+      letters = 2.times.map { ('A'..'Z').to_a.sample }.join
+      digits = format('%03d', rand(1000))
+      name = letters + digits
+      unless @@list_of_names.include?(name)
+        @@list_of_names << name
+        return name
+      end
+    end
+  end
+
+  def name
+    @name
+  end
+
+  def reset
+    @@list_of_names.delete(@name)
+    @name = generate_unique_name
+  end
+end
