eed387c1852e0f3c79e4430730f1e0fa450ba3c3
Elchunen Freund <elchunenfreund@gmail.com>
2025-05-22 01:24:29 -0400

    Added chapter 1

--- RB175/book_viewer/book_viewer.rb
+++ RB175/book_viewer/book_viewer.rb
@@ -1,6 +1,19 @@
+require "tilt/erubi"
 require "sinatra"
 require "sinatra/reloader"
 
 get "/" do
-  File.read "public/template.html"
+  # File.read "public/template.html"
+  @title = "The Adventures of Sherlock Holmes"
+  @contents = File.readlines("data/toc.txt")
+
+  erb :home
+end
+
+get "/chapters/1" do
+  @title = "Chapter 1"
+  @contents = File.readlines("data/toc.txt")
+  @chapter = File.readlines("data/chp1.txt")
+
+  erb :chapter
 end
