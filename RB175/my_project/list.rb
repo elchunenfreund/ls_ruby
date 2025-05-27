f0bc02af60315a2d49f25bf345e537b2c6f53979
Elchunen Freund <elchunenfreund@gmail.com>
2025-05-27 00:52:10 -0400

    added coments to the list files

--- RB175/my_project/list.rb
+++ RB175/my_project/list.rb
@@ -2,8 +2,8 @@ require "sinatra"
 require "sinatra/reloader"
 require "tilt/erubi"
 
-get "/" do
-  @files = Dir.glob("public/*").map {|file| File.basename(file) }.sort
-  @files.reverse! if params[:sort] == "desc"
+get "/" do # basic routing method that creates a files variable that will be used in the list erb file.
+  @files = Dir.glob("public/*").map {|file| File.basename(file) }.sort # sorts the files
+  @files.reverse! if params[:sort] == "desc" # reverses the if the params variable is assigned to do so.
   erb :list
 end
