2113b635709d06b307ae87266583bf22ffea20c9
Elchunen Freund <elchunenfreund@gmail.com>
2025-05-29 18:41:01 -0400

    Started with the todo project

---
+++ RB175/todo_project/todo.rb
@@ -0,0 +1,30 @@
+require "sinatra"
+require "sinatra/reloader"
+require "tilt/erubi"
+
+configure do
+  enable :sessions
+  set :session_secret, SecureRandom.hex(32)
+end
+
+before do
+  session[:lists] ||= []
+end
+
+get "/" do
+  redirect "/lists"
+end
+
+get "/lists" do
+  @lists = session[:lists]
+  erb :lists, layout: :layout
+end
+
+get "/lists/new" do
+  erb :new_list, layout: :layout
+end
+
+post "/lists"
+  session[:lists] << { name: perams[:list_name], todos: [] }
+  redirect "/lists"
+end
