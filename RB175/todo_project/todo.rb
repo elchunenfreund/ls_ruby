65f6020f7fc1b19129dec357c1ab57fa741b3087
Elchunen Freund <elchunenfreund@gmail.com>
2025-06-08 18:25:30 -0400

    added functionality to the todos

--- RB175/todo_project/todo.rb
+++ RB175/todo_project/todo.rb
@@ -123,5 +123,19 @@ post '/list/:list_id/todos/:todo_id/destroy' do
   todo_id = params[:todo_id].to_i
   @list = session[:lists][@list_id]
   @list[:todos].delete_at(todo_id)
+  session[:success] = "The todo has been deleted"
+  redirect "/lists/#{@list_id}"
+end
+
+# Update the status of a todo
+post '/list/:list_id/todos/:todo_id' do
+  @list_id = params[:list_id].to_i
+  @list = session[:lists][@list_id]
+
+  todo_id = params[:todo_id].to_i
+  is_completed = params[:completed] == "true"
+  @list[:todos][todo_id][:completed] = is_completed
+
+  session[:success] = "The todo has been updated"
   redirect "/lists/#{@list_id}"
 end
