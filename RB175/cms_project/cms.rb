d3e0c351c778c8c02cd5e76fe6a26931fd1fa026
Elchunen Freund <elchunenfreund@gmail.com>
2025-06-18 10:27:35 -0400

    working on the tests

--- RB175/cms_project/cms.rb
+++ RB175/cms_project/cms.rb
@@ -1,6 +1,20 @@
 require "sinatra"
 require "sinatra/reloader"
+require "tilt/erubi"
+
+root = File.expand_path("..", __FILE__)
 
 get "/" do
-  "Getting started."
+  @files = Dir.glob(root + "/data/*").map do |path|
+    File.basename(path)
+  end
+
+  erb :index
+end
+
+get "/data/:filename" do
+  file_path = root + "/data/" + params[:filename]
+
+  headers["Content_Type"] = "text/plain"
+  File.read(file_path)
 end
