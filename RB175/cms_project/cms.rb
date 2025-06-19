56f4fbacd1cb88b97758154834c28a3519226f2c
Elchunen Freund <elchunenfreund@gmail.com>
2025-06-19 18:19:19 -0400

    Working on the file system and tets

--- RB175/cms_project/cms.rb
+++ RB175/cms_project/cms.rb
@@ -1,20 +1,77 @@
 require "sinatra"
 require "sinatra/reloader"
 require "tilt/erubi"
+require "redcarpet"
 
 root = File.expand_path("..", __FILE__)
 
+configure do
+  enable :sessions
+  set :session_secret, SecureRandom.hex(32)
+end
+
+def data_path
+  if ENV["RACK_ENV"] == "test"
+    File.expand_path("../test/data", __FILE__)
+  else
+    File.expand_path("../data", __FILE__)
+  end
+end
+
+# Render markdown.
+def render_markdown(text)
+  markdown = Redcarpet::Markdown.new(Redcarpet::Render::HTML)
+  markdown.render(text)
+end
+
+# Load the file in the correct format
+def load_file_contant(path)
+  content = File.read(path)
+  case File.extname(path)
+  when ".txt"
+    headers["Content-Type"] = "text/plain"
+    content
+  when ".md"
+    render_markdown(content)
+  end
+end
+
+# Home page, list of files.
 get "/" do
-  @files = Dir.glob(root + "/data/*").map do |path|
+  pattern = File.join(data_path, "*")
+  @files = Dir.glob(pattern).map do |path|
     File.basename(path)
   end
 
   erb :index
 end
 
-get "/data/:filename" do
-  file_path = root + "/data/" + params[:filename]
+# view file, and throw error if file doesnt exist
+get "/:filename" do
+  file_path = File.join(data_path, params[:filename])
 
-  headers["Content_Type"] = "text/plain"
-  File.read(file_path)
+  if File.exist?(file_path)
+    load_file_contant(file_path)
+  else
+    session[:message] = "#{params[:filename]} does not exist."
+    redirect "/"
+  end
+end
+
+# Edit file
+get "/:filename/edit" do
+  file_path = File.join(data_path, params[:filename])
+
+  @filename = params[:filename]
+  @content = File.read(file_path)
+  erb :edit
+end
+
+post "/:filename" do
+  file_path = File.join(data_path, params[:filename])
+
+  File.write(file_path, params[:content])
+
+  session[:message] = "#{params[:filename]} has been updated."
+  redirect "/"
 end
